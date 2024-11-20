d38f032d06802c1fe6b45ae3cec2acc43e0a9f85
202102187e <202102187e@utea.edu.pe>
2024-11-20 07:36:19 -0500

    Variables NUmericas

---
+++ variables.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+// Los numeros enteros solo admiten enteros y reales 
+int miEntero = 2 ;
+double miReal = 3.141516;
+
+print (miEntero);
+
+print (miEntero.toString());
+
+print (miEntero+ miEntero);
+
+print(miReal.toInt());
+
+var x = 1;
+var y= "2";
+//Una forma de declarar varibles sin un tipo de dato definido
+
+print(x);
+print(y);
+
+
+
+
+
+
+
+
+}
