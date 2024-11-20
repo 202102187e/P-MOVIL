4d002148a2e86375f53167d6ea7b6f0841f7af48
202102187e <202102187e@utea.edu.pe>
2024-11-20 07:44:55 -0500

    Variables tipo cadena

---
+++ variables1.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  // manejo de cadenas o de caracteres ,puede ir
+  // cadenas simples o dobles 
+
+String miCadena = 'Hola soy Max';
+print(miCadena);
+print(miCadena.length); //Longitud de caracteres
+
+print(miCadena.toLowerCase()); // Todo minuscula
+
+print(miCadena.toUpperCase()); // Todo mayuscula
+
+print(miCadena.substring(1,4)); // Extrae cadena en un rango defindo
+
+
+
+
+}
