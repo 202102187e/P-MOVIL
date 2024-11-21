4d6564357854e67fca720c23c5f96b7565166b4d
202102187e <202102187e@utea.edu.pe>
2024-11-21 07:35:24 -0500

    bucles

---
+++ bucles.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  // Estructuras Repetitivas ,permiten iterar sobre una coleccion de elementos
+  // for, foreach ,while, do while
+
+
+  for (var i = 0; i < 5; i++) {
+    print("i vale $i");
+  }
+
+  int i =0;
+  while (i<5) {
+    print("(while)  es $i");
+    i++;
+  }
+
+  int c =0 ;
+
+  do {
+    print("(do while) es $c");
+    c++;
+  } while (c<5);
+
+
+
+
+
+
+}
